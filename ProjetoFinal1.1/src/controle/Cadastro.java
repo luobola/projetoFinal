a2aade869b4c57f78316b689cafdc7ebfec25273
luobola <51456985+luobola@users.noreply.github.com>
2019-06-06 20:16:49 -0300

    Commit atualizado
    Co-Authored-By: Lucas <lucas@users.noreply.github.com>

--- ProjetoFinal1.1/src/controle/Cadastro.java
+++ ProjetoFinal1.1/src/controle/Cadastro.java
@@ -1,7 +1,45 @@
 package controle;
 
 
+import Entity.Albuns;
+import Entity.AutorBanda;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
 public class Cadastro {
+
+    @FXML
+    private TextField tfNome;
+    @FXML
+    private TextField tfCidadeOrigem;
+    @FXML
+    private TextField tfAnoNascimento;
+
+    @FXML
+    private TextField tfNome1;
+    @FXML
+    private TextField tfAnoLancamento;
+
+
+
+
+
+
+    public AutorBanda coletaInformacao(){
+        String nome = tfNome.getText();
+        String cidadeOrigem = tfCidadeOrigem.getText();
+        double anoNascimento = Double.parseDouble(tfAnoNascimento.getText());
+
+        AutorBanda ab = new AutorBanda(nome, cidadeOrigem, anoNascimento);
+
+        return ab;
+    }
+
+    public Albuns coletaAlbuns(){
+
+
+
+    }
 
 
 
--- ProjetoFinal1.1/src/controle/JanelaPrincipal.java
+++ ProjetoFinal1.1/src/controle/JanelaPrincipal.java
@@ -1,6 +1,49 @@
 package controle;
 
+import Entity.AutorBanda;
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Dialog;
+
+import java.io.IOException;
+import java.util.Optional;
+
 public class JanelaPrincipal {
 
+
+    @FXML
+    private void acaoCadastraBanda(){
+        Dialog<ButtonType> dialog = new Dialog<>();
+
+        try{
+            FXMLLoader loader = new FXMLLoader();
+
+            loader.setLocation(getClass().getResource("cadastroBanda.fxml"));
+
+            Parent content = loader.load();
+            dialog.getDialogPane().setContent(content);
+            dialog.getDialogPane().getButtonTypes().add(ButtonType.APPLY);
+            dialog.getDialogPane().getButtonTypes().add(ButtonType.CANCEL);
+
+            Optional<ButtonType> resultado = dialog.showAndWait();
+
+            if(resultado.isPresent() && resultado.get() == ButtonType.APPLY){
+                Cadastro controle = loader.getController();
+
+                AutorBanda autorBanda = controle.coletaInformacao();
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    private void sair(){
+        Platform.exit();
+    }
 
 }
